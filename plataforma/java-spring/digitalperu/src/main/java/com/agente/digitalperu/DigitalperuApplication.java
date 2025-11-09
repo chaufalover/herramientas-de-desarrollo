4f15957fdcaede067309dfeae836c84cb11a36c2
chaufalover <tacuresancheztomas@gmail.com>
2025-11-09 17:58:32 -0500

    Estructura carpetas: Creacion de carpeta palataforma + proyecto Java con SpringBoot

---
+++ plataforma/java-spring/digitalperu/src/main/java/com/agente/digitalperu/DigitalperuApplication.java
@@ -0,0 +1,13 @@
+package com.agente.digitalperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalperuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalperuApplication.class, args);
+	}
+
+}
---
+++ plataforma/java-spring/digitalperu/src/test/java/com/agente/digitalperu/DigitalperuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agente.digitalperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalperuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
