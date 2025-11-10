6dfbcdb513fbc6e303af5529048dcf1dbf330472
chaufalover <tacuresancheztomas@gmail.com>
2025-11-09 22:42:48 -0500

    QR: Generador de qr mas validacion en deteccion

---
+++ plataforma/py-services/qr/generator.py
@@ -0,0 +1,25 @@
+import qrcode
+
+def generar_qr():
+   
+    numero = input("Ingrese el número que desea convertir en QR: ")
+
+    qr = qrcode.QRCode(
+        version=1, 
+        error_correction=qrcode.constants.ERROR_CORRECT_L, 
+        box_size=10,  
+        border=4,  
+    )
+
+    qr.add_data(numero)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    nombre_archivo = f"qr_{numero}.png"
+    img.save(f"/home/tom/Pictures/{nombre_archivo}")
+
+    print(f"\n✅ Código QR generado y guardado como: {nombre_archivo}")
+
+
+generar_qr();
